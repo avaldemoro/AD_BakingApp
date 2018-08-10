9fde5f0f19c66641e46036a6c065d31f4af3c856
Aster Valdemoro <astervaldemoro@gmail.com>
2018-08-10 09:42:41 -0400

    implement espresso and ran tests

---
+++ app/src/androidTest/java/co/asterv/ad_bakingapp/BakingAppGeneralTests.java
@@ -0,0 +1,31 @@
+package co.asterv.ad_bakingapp;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class BakingAppGeneralTests {
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickRecipeItem_OpensDetailFragment() {
+        // Find the view & Perform action on view
+        onView((withId(R.id.recipeNameRecyclerView)))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        // Check if the view does as expected
+        onView(withId(R.id.recipe_detail_container)).check(matches(isDisplayed ()));
+    }
+}
